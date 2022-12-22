bceefde651b0e3328a6d6ce36b723a12077eecde
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-12-22 11:05:29 +0900

    table sample

--- front/pages/api/hello.ts
+++ front/pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
-}
+type Article = {
+  id: string;
+  title: string;
+  auther: string;
+  media: string;
+  url: string;
+  summary: string;
+  created_at: string;
+  crawled_at: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
