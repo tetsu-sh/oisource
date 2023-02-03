160a2d3dee6ee050c293740e0c97cd3db54d778d
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2023-02-03 22:23:20 +0900

    port change

--- front/pages/api/apolloClient.ts
+++ front/pages/api/apolloClient.ts
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:8000",
+    uri: "http://localhost:8080",
   }),
   cache: new InMemoryCache(),
 });
